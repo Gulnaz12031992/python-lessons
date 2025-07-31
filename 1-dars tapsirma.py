660c54a30e783b493b37cf12b5b0be83b306df75
Gulnaz12031992 <gulnazmaulenbergenova92@gmail.com>
2025-07-31 07:36:30 +0300

    janalandi
    1-tapsirma

---
+++ 1-dars tapsirma.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 24 09:02:20 2025
+
+@author: Admin
+"""
+
+print('5 din\' da\'rejesin tabin\'', '\njuwap:', 5*5)
+print('22 ni 4 ke bo\'lgende qansha qaldiq qaladi?', '\njuwabi:',22%4)
+print('ta\'repi 125 ke ten\' bolg\'an kvadrattin\' perimetri ha\'m maydanin tabin\'', '\njuwabi:', '\nperimetri:', 4*125, '\nmaydani:',4*125)
